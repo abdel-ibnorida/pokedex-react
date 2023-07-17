3774cb25a42391049348ebb5929df4081816a19f
abdelibnorida <abdel.ibnorida@kappas.it>
2023-07-17 18:54:36 +0200

    add17-07-23

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import Cards from './Components/Cards'
 import Navbar from './Components/Navbar'
+import { createContext, useContext } from 'react'
+export const UserContext = createContext();
 
 function App() {
-
+  
   return (
-    <>
+    <UserContext.Provider value = {'utente loggato'}>
     <Navbar/>
     <Cards/>
     pokedex
-    </>
+    </UserContext.Provider>
   )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
 import './Navbar.css'
 import image1 from "../../assets/img/pokemon-logo-png-1428.png";
+import { UserContext } from '../../App';
+import { useContext } from 'react';
+
 
 const Navbar =() => {
+    const valore = useContext(UserContext);
     return(
         <div className='navbar'>
                 <img src={image1} alt="logo" />
+                <div> {valore}</div>
         </div>
     )
 }
